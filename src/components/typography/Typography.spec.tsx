3f07a8d19b4fb306e08237a0f31ef9faa4297670
Richard Ben <frichardben@yahoo.com.br>
2022-03-18 09:27:09 -0300

    feat: added component typography

---
+++ src/components/typography/Typography.spec.tsx
@@ -0,0 +1,32 @@
+import { Typography, ITypographyProps } from './Typography'
+import { render, screen } from '@testing-library/react'
+
+describe('Typography', () => {
+  let props: ITypographyProps
+
+  beforeEach(() => {
+    props = {
+      variant: 'h3',
+      color: 'gray',
+      children: 'Teste 1'
+    }
+  })
+
+  it('should render a function', () => {
+    expect(typeof Typography)
+  })
+
+  it('should be render without crashing', () => {
+    const { container } = render(<Typography {...props} />)
+    expect(container).toBeInTheDocument()
+
+    expect(container).toHaveTextContent('Teste 1')
+  })
+
+  it('should be render a text', () => {
+    render(<Typography {...props} />)
+    const text = screen.getByText('Teste 1')
+
+    expect(text).toBeInTheDocument()
+  })
+})
